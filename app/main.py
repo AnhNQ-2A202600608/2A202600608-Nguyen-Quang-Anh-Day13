3a20d7dfeeca0b9d6ce8718b3aecf2330b9eaaad
AnhNQ-2A202600608 <quanganhptit030@gmail.com>
2026-06-15 14:50:54 +0700

    Remove screenshot helper

--- app/main.py
+++ app/main.py
@@ -14,13 +14,11 @@ from .middleware import CorrelationIdMiddleware
 from .pii import hash_user_id, summarize_text
 from .schemas import ChatRequest, ChatResponse
 from .tracing import tracing_enabled
-from .screenshot_helper import router as screenshot_router
 
 configure_logging()
 log = get_logger()
 app = FastAPI(title="Day 13 Observability Lab")
 app.add_middleware(CorrelationIdMiddleware)
-app.include_router(screenshot_router)
 agent = LabAgent()
 
 
